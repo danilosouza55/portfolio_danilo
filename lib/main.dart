9174171927d85be2cbd1161522314d8eee4eedc0
Danilo <danilo.asouza@hotmail.com>
2024-03-04 22:43:45 -0300

    melhorias, icone e ajustes

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:portfolio_danilo/src/app_widget.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const AppWidget());
 }
