807aeb46eb56dbc7e61dd1dd484f6c31b4e1def4
Danilo Souza <danilo.souza@hotmail.com.br>
2025-08-15 00:46:40 -0300

    v3.6.1

--- android/app/src/main/kotlin/br/dev/danilosouza/portfolio_danilo/MainActivity.kt
+++ android/app/src/main/kotlin/br/dev/danilosouza/portfolio_danilo/MainActivity.kt
@@ -1,6 +1,28 @@
 package br.dev.danilosouza.portfolio_danilo
 
+import android.os.Bundle
+import android.os.Build
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // Configura a decoração da janela para edge-to-edge
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        
+        // Usa as novas APIs para Android 15+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            // Obtém o controlador de WindowInsets
+            val windowInsetsController = WindowCompat.getInsetsController(window, window.decorView)
+            
+            // Define a visibilidade e aparência das barras do sistema
+            windowInsetsController.apply {
+                isAppearanceLightStatusBars = false // ou true, dependendo do seu tema
+                isAppearanceLightNavigationBars = false // ou true, dependendo do seu tema
+            }
+        }
+        
+        super.onCreate(savedInstanceState)
+    }
 }
